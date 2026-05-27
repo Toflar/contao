0be0f9324f9e78e565f7be367ee9e5d18f56d52b
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2026-05-27 16:51:40 +0200

    Re-add the BC layer to the sortables controller

--- core-bundle/assets/controllers/sortable-controller.js
+++ core-bundle/assets/controllers/sortable-controller.js
@@ -29,6 +29,27 @@ export default class extends Controller {
         }
 
         this.sortable = new Sortable(this.element, options);
+
+        // Backwards compatibility for parent mode, will unhide the operation if no other drag handle is found (remove in Contao 6)
+        for (const el of [...this.element.children]) {
+            const handles = el.querySelectorAll('.drag-handle');
+
+            // If there are less than two handles, we are not in BC mode
+            if (handles.length <= 2) {
+                continue;
+            }
+
+            // There will always be at least 2 handles: one for the operations list and one for the operations menu (which is hidden)
+            if (handles.length === 2) {
+                handles[0].style.display = '';
+            }
+
+            for (const handle of handles) {
+                if (handle.style.display === 'none' && handle.parentNode.localName === 'li') {
+                    handle.parentNode.style = 'display: none !important';
+                }
+            }
+        }
     }
 
     disconnect() {
